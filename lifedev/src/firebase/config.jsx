28d2631b018e718b0d7313bc895c06164b66d446
RafaelTabolka <rafaeltabolka44@gmail.com>
2023-11-16 22:43:59 -0300

    Aula 16/11 - Conectores do Firebase

--- lifedev/src/firebase/config.jsx
+++ lifedev/src/firebase/config.jsx
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2AWPVq95_j6v3_ScpMaoIwDvFej2RjmE",
-  authDomain: "lifedev-prof.firebaseapp.com",
-  projectId: "lifedev-prof",
-  storageBucket: "lifedev-prof.appspot.com",
-  messagingSenderId: "616028756636",
-  appId: "1:616028756636:web:e4dfb0835dd4af800a2b59",
-  measurementId: "G-2TMMM1DM2N"
+  apiKey: "AIzaSyAWJ5Mr0FqJMONo9JUUAmQkgUzEM5MXahI",
+  authDomain: "lifedev-rafael-priscila.firebaseapp.com",
+  projectId: "lifedev-rafael-priscila",
+  storageBucket: "lifedev-rafael-priscila.appspot.com",
+  messagingSenderId: "407382441048",
+  appId: "1:407382441048:web:688d1a5b091438d98a9e2d",
+  measurementId: "G-PSW0E8QCDG"
 };
 
 const app = initializeApp(firebaseConfig);
